d085d8413e411653a45f98d9ab6de83c3b65e5bb
haqi <hqq.amrullah@gmail.com>
2025-07-18 13:36:08 +0700

    fix: redirect url

--- src/controllers/shortener.controller.ts
+++ src/controllers/shortener.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import * as urlService from '../services/ulr.service';
+import * as urlService from '../services/url.service';
 
 export const shortenUrl = async (req: Request, res: Response) => {
   const { originalUrl, customAlias } = req.body;
@@ -13,7 +13,9 @@ export const redirectToOriginalUrl = async (req: Request, res: Response) => {
 
   const originalUrl = await urlService.getOriginalUrl(alias);
   if (typeof originalUrl === 'string') {
-    res.redirect(originalUrl);
+    res.setHeader('Cache-Control', 'no-store');
+    res.setHeader('Referrer-policy', 'no-referrer')
+    res.redirect(301, originalUrl);
   } else {
     res.status(404).json({ error: 'URL not found' });
   }
